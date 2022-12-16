493f496c6009e8ef5e5f61227638fbace4bdd66c
Sanjay Chauhan <sanjay@nitsan.in>
2022-12-16 21:30:39 +0530

    [TASK] Improved configuration and feature-rich

--- README.md
+++ README.md
@@ -1,3 +1,104 @@
-# vanilla-site-speed-booster
+# vanilla-site-speed-booster 
 
-Vanilla Site Speed Booster
+> Boostup your website without any modern FancyJS framework like ReactJS, VueJS etc., Your website will get best speed & user experiance with features like Without reloading site, Dynamically apply AJAX etc.
+
+**Features**:
+- Speed-up your website
+- Light weight plugin (~5KB)
+- Zero-coding
+- Easy to install & configure
+- Quickly launch with simple usage
+
+**Advantages**:
+- Best user-experiance to website visitors
+- Just like Mobile native app
+- Sped-up your website
+- Get best ranking speed at Page-speed
+- No more FancyJS like Reactjs
+
+## Table of Contents
+
+- [Demo](https://t3planet.com)
+- [Install](#install)
+- [Usage](#usage)
+- [Browser Support](#browser-support)
+- [FAQs](#faqs)
+- [Contribute](#contribute)
+- [Changelog](#changelog)
+- [License](#license)
+
+## Install
+
+```sh
+# You can install lozad with npm
+$ npm i @nitsantechnologies/vanilla-site-speed-booster
+
+# Alternatively you can use Yarn
+$ yarn add @nitsantechnologies/vanilla-site-speed-booster
+```
+
+## Usage
+
+Then with a module bundler like rollup or webpack, use as you would anything else:
+
+```javascript
+// using ES6 modules
+import VanillaSiteSpeedBooster from '@nitsantechnologies/vanilla-site-speed-booster';
+
+// using CommonJS modules
+const yourVariable = new VanillaSiteSpeedBooster({
+
+  // If you need progress bar then enabled, We use famous nprogress.js (know more at below FAQ section)
+  enableProgressBar: false,
+
+  // Set Id's of your Bundlejs script tag (know more at below usage section)
+  idBundleJs: 'pageAjax',
+
+  // Enter list of URLs which you want to exclude - Remove this speed-booster feature
+  excludeUrls: '/exclude-page',
+
+  // Enter list of CSS-selector which you want to exclude of Anchor tag - Remove this speed-booster feature
+  langSwitch: '.myElement a',
+
+  // Enter list of CSS-selector which you want to exclude from whole page - Remove this speed-booster feature
+  removeUsingPageClass: '.myCustomPageClass',
+
+  // Add Error Message
+  errorMsg: 'Oops! Fatal error in VanillaSiteSpeedBooster plugin',
+
+  // CSS-class name of your site's Main div-wrapper
+  mainClassName: '.site-main',
+
+  // Enable/Disable Browser's back & forward feature
+  pageBackForwardReload: true,
+
+  // If you have remove this functionality by target specific class
+  removeUsingTargetClass: 'lang-menu-item',
+});
+```
+
+## Browser Support
+
+Available in [latest browsers](http://caniuse.com/#feat=intersectionobserver).
+
+## FAQs
+
+- Please don't use `DOMContentLoaded`
+- To enable Progress-bar, Please install https://www.npmjs.com/package/nprogress
+
+- We are supporting below Modalbox/Lightbox plugins, You don't need to configure anything. We will dynamically grab it.
+- Fancybox
+- Bootstrap 5 Modal
+- Glightbox
+- Colorbox
+- Darkbox
+
+## Contribute
+
+Interested in contributing features and fixes?
+
+[Excited to receive your PR](https://github.com/nitsan-technologies/ns_vanilla_site_speed_bootster).
+
+## License
+
+[MIT](LICENSE) © [NITSAN Technologies](https://nitsantech.com)
--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 class VanillaSiteSpeedBooster {
   constructor(config) {
     this.options = {
-      attributeLightboxPlugin: 'data-fancybox',
-      excludeExternalLink: true,
       excludeUrls: '/blog',
-      enableHashtag: true,
-      enableHashUrlReload: true,
-      enableProgressBar: true,
-      addBundleJsPath: '/typo3conf/ext/ns_theme_child/Resources/Public/dist/assets/js/theme.bundle.js',
-      idBundleJsPath: 'pageAjax',
+      enableProgressBar: false,
+      idBundleJs: 'pageAjax',
+      langSwitch: '.header-language-menu a',
+      mainClassName: '.site-main',
+      pageBackForwardReload: true,
+      removeUsingPageClass: '.offer-page-header',
+      removeUsingTargetClass: 'lang-menu-item',
       errorMsg: 'Something is went wrong for NsWebsiteWithoutReload plugin',
-      mainClassName: 'site-main',
-      pageBackReload: true,
-      removeFromPage: 'offer-page-header',
+      headerCollapse: false,
     };
 
     if (typeof config === 'object') {
@@ -26,17 +24,17 @@ class VanillaSiteSpeedBooster {
   }
 
   init() {
-    const mainSection = document.querySelector(`.${this.options.mainClassName}`);
+    const mainSection = document.querySelector(`${this.options.mainClassName}`);
     const menuLinks = document.querySelectorAll('a:not([target])');
 
     // If user click on back button page was reload
-    window.onpopstate = () => {
-      window.location.reload();
-    };
+    if (this.options.pageBackForwardReload) {
+      window.onpopstate = () => {window.location.reload();};
+    }
 
-    if (menuLinks.length && mainSection && !document.querySelector(`.${this.options.removeFromPage}`)) {
+    if (menuLinks.length && mainSection && !document.querySelector(`${this.options.removeUsingPageClass}`)) {
       menuLinks.forEach((i) => {
-        if (!i.hasAttribute(`${this.options.attributeLightboxPlugin}`)) {
+        if (!i.hasAttribute('data-fancybox')) {
           i.addEventListener('click', (event) => {
             menuLinks.forEach((j) => {
               j.parentElement.classList.remove('active');
@@ -47,9 +45,8 @@ class VanillaSiteSpeedBooster {
       });
 
       document.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (event.target.tagName === 'A' && !event.target.hasAttribute('target') && !event.target.hasAttribute(`${this.options.attributeLightboxPlugin}`)) {
-          if (event.target.href.split('#').length === 1 && event.target.href.indexOf(`${this.options.excludeUrls}`) === -1 && !event.target.hasAttribute(`${this.options.attributeLightboxPlugin}`)) {
+        if (event.target.tagName === 'A' && !event.target.hasAttribute('target') && !event.target.hasAttribute('data-fancybox')  && !event.target.hasAttribute('data-bs-toggle', 'modal') && !event.target.hasAttribute('data-glightbox') && !event.target.hasAttribute('data-darkbox') && !event.target.className.includes('cboxElement') ) {
+          if (event.target.href.split('#').length === 1 && event.target.href.indexOf(`${this.options.excludeUrls}`) === -1 && !event.target.className.includes(this.options.removeUsingTargetClass)) {
             event.preventDefault();
             document.body.classList.add('ns-website-content');
             if (this.options.enableProgressBar) {
@@ -65,52 +62,54 @@ class VanillaSiteSpeedBooster {
               });
             }
 
-            const menuTrigger = document.querySelector('#menuTrigger');
-            const bodyDiv = document.querySelector('body');
-            bodyDiv.classList.remove('menu--open');
-            menuTrigger.addEventListener('click', () => {
-              bodyDiv.classList.toggle('menu--open');
-            });
+            if (this.options.headerCollapse) {
+              const menuTrigger = document.querySelector('#menuTrigger');
+              const bodyDiv = document.querySelector('body');
+              bodyDiv.classList.remove('menu--open');
+              menuTrigger.addEventListener('click', () => {
+                bodyDiv.classList.toggle('menu--open');
+              });
 
-            if (event.target.parentElement.classList.contains('has-sub')) {
-              event.target.parentNode.classList.toggle('active');
-              event.target.parentNode.classList.toggle('slide--up');
+              if (event.target.parentElement.classList.contains('has-sub')) {
+                event.target.parentNode.classList.toggle('active');
+                event.target.parentNode.classList.toggle('slide--up');
+              }
             }
 
             const url = event.target.href;
-            fetch(url)
-              .then((response) => response.text())
-              .then((html) => {
-                /* Change Content of Main */
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const mainContent = doc.querySelector(`.${this.options.mainClassName}`);
-                mainSection.classList.add('fade');
-                setTimeout(() => {
-                  mainSection.innerHTML = mainContent.innerHTML;
-                  mainSection.className = mainContent.className;
-                  window.history.pushState(null, null, url);
-                  document.querySelector('title').innerHTML = doc.title;
+            fetch(url, {cache: "force-cache"}).then((response) => response.text()).then((html) => {
+              /* Change Content of Main */
+              const parser = new DOMParser();
+              const doc = parser.parseFromString(html, 'text/html');
+              const mainContent = doc.querySelector(`${this.options.mainClassName}`);
+              const langLink = document.querySelector(`${this.options.langSwitch}`);
+              mainSection.classList.add('fade');
+              setTimeout(() => {
+                mainSection.innerHTML = mainContent.innerHTML;
+                mainSection.className = mainContent.className;
+                window.history.pushState(null, null, url);
+                document.querySelector('title').innerHTML = doc.title;
 
-                  if (this.options.enableProgressBar) {
-                    NProgress.done();
-                  }
+                if (langLink.href && doc.querySelector(`${this.options.langSwitch}`) && doc.querySelector(`${this.options.langSwitch}`).href) {
+                  langLink.href = doc.querySelector(`${this.options.langSwitch}`).href;
+                }
 
+                if (this.options.enableProgressBar) {
+                  NProgress.done();
+                }
+
+                if (this.options.idBundleJs) {
                   const script = document.createElement('script');
-                  script.src = this.options.addBundleJsPath;
+                  script.src = document.getElementById(`${this.options.idBundleJs}`).src;
 
                   // append and execute script
                   mainSection.append(script);
-
-                  if (document.getElementById(`${this.options.idBundleJsPath}`)) {
-                    document.getElementById(`${this.options.idBundleJsPath}`).remove();
-                  }
-                }, 300);
-              })
-              .catch((err) => {
+                }
+              }, 300);
+            }).catch((err) => {
                 console.warn(this.options.errorMsg, err);
               });
-          } else if (event.target.getAttribute('href').charAt(0) !== '#') {
+          } else if (event.target.getAttribute('href').charAt(0) !== '#' && event.target.className === 'lang-menu-item') {
             window.location.href = event.target.href;
           }
         }
@@ -119,4 +118,4 @@ class VanillaSiteSpeedBooster {
   }
 }
 
-export default NsWebsiteWithoutReload;
+export default VanillaSiteSpeedBooster;
